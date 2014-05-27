c608f86ebca0bb8e3dcd1df7cdb99afc3a85cf53
Kazumi <kazumi.ah@gmail.com>
2014-05-27 10:56:38 -0400

    Monty Python and the Holy Grail!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SecondApp::Application.config.secret_key_base = '26dcf8c3c2b03ca2f5636ab04c7692f5d5d890ebc18e4da11164a6c15214b154626765902d7856f75e400ec87246b0cf8412b5e7fbab659b03b2a4db4d88d3f9'
